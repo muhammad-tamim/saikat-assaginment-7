7f038c018b1d3ba97892bb98c154e045fa56ccc6
Muhammad Tamim <mdt40234@gmail.com>
2024-03-15 23:56:21 +0600

    navbar completed

---
+++ component/nav/nav.jsx
@@ -0,0 +1,24 @@
+import './nav.css'
+const Nav = () => {
+    return (
+        <div className="nav">
+            <div className='logo'>Food Recipe</div>
+            <div className='nav-links'>
+                <a href="/">Home</a>
+                <a href="#">Recipes</a>
+                <a href="#">About</a>
+                <a href="#">Search</a>
+            </div>
+            <div className='search-box'>
+                <div>
+                    <input placeholder='Search' type="text" />
+                </div>
+                <div>
+                    <img src="../../src/assets/img/profile.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from '../component/nav/nav';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  )
+}
+
+export default App;
